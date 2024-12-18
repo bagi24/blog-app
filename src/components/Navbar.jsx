31ccdc813474a237609bcdcae61e556a76297032
bagi24 <bagrat.injgia24@gmail.com>
2024-12-18 12:32:31 +0400

    instal and added Clerk Authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Image from './Image';
+import { Link } from 'react-router-dom';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -7,10 +9,10 @@ const Navbar = () => {
   return (
     <div className=' w-full h-16 md:h-20 flex items-center justify-between'>
       {/*LOGO */}
-      <div className=' flex items-center gap-4 text-2xl font-bold'>
+      <Link to='/' className=' flex items-center gap-4 text-2xl font-bold'>
         <Image src='logo.png' alt='Bagi Logo' w={32} h={32} />
         <span>bagilog</span>
-      </div>
+      </Link>
       {/*MOBILE MENU */}
       <div className=' md:hidden'>
         {/*Mobile Button */}
@@ -22,24 +24,29 @@ const Navbar = () => {
           className={` w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out  ${
             open ? '-right-0' : '-right-[100%]'
           }  `}>
-          <a href='/'>Home</a>
-          <a href='/'>Trending</a>
-          <a href='/'>Most Popular</a>
-          <a href='/'>About</a>
-          <a href='/'>
+          <Link to='/'>Home</Link>
+          <Link to='/'>Most Popular</Link>
+          <Link to='/'>About</Link>
+          <Link to='/'>
             <button className=' py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login👋 </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/*DESCTOP MENU */}
       <div className=' hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
-        <a href='/'>Home</a>
-        <a href='/'>Trending</a>
-        <a href='/'>Most Popular</a>
-        <a href='/'>About</a>
-        <a href='/'>
-          <button className=' py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login👋 </button>
-        </a>
+        <Link to='/'>Home</Link>
+        <Link to='/'>Trending</Link>
+        <Link to='/'>Most Popular</Link>
+        <Link to='/'>About</Link>
+
+        <SignedOut>
+          <Link to='/login'>
+            <button className=' py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login👋 </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -9,36 +9,51 @@ import LoginPage from './routes/LoginPage.jsx';
 import Register from './routes/Register.jsx';
 import SinglePostPage from './routes/SinglePostPage.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Homepage />,
-  },
-  {
-    path: '/posts',
-    element: <PostListPage />,
-  },
-  {
-    path: '/:slug',
-    element: <SinglePostPage />,
-  },
-  {
-    path: '/write',
-    element: <Write />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />,
+      },
+      {
+        path: '/posts',
+        element: <PostListPage />,
+      },
+      {
+        path: '/:slug',
+        element: <SinglePostPage />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>
 );
